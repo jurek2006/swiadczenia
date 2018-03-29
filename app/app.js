160766842526dce4e51d645dc4ca829a5c84baea
Jurek <jurek2006@gmail.com>
2018-03-29 06:49:20 +0200

    Przeniesienie czytania danych z pliku do promisy i przetwarzania go na tablicę obiektów wizyt do funkcji.

--- app/app.js
+++ app/app.js
@@ -2,38 +2,54 @@ const _ = require('lodash')
 const fs = require('fs');
 const bcrypt = require('bcrypt');
 
+const readDataFromFile = (pathToFile) => {
+	return new Promise((resolve, reject) =>{
+		fs.readFile(pathToFile, 'utf8', (err, data) => {
+			if (err) reject(err);
 
-fs.readFile('../data/data.csv', 'utf8', (err, data) => {
-	if (err) throw err;
-	
-	const readData = data.split("\r\n");
+			resolve(data);
+		});
+	});
+}
+
+const convertReadDataToVisitsArr = (readTextData) => {
+
+	const visitsInLines = readTextData.split("\r\n"); //utworzenie tablicy, gdzie element to (text) danych poszczególnej wizyty
 	// tutaj weryfikacja pierwszego wiersza (nagłówka) - DODAĆ
-	readData.shift(); //usunięcie nagłówka
-	const outputData = readData.map(line => {
-		const split = line.split("\t");
+	visitsInLines.shift(); //usunięcie nagłówka	
+
+	// przetworzenie danych wizyt na tablicę obiektów (gdzie każdy obiekt zawiera dane poszczególnej wizyty)
+	// rozdzielenie poszczególnych pól względem tabulacji
+	const visitsArr = visitsInLines.map(line => {
+		const visit = line.split("\t");
 		// zczytanie wszystkich procedur I10 dla świadczenia
 		const icd10 = [];
 		for(let i=0; i < 4; i++){
-			if(split[4+i] !== ''){
-				icd10.push(split[4+i]);
+			if(visit[4+i] !== ''){
+				icd10.push(visit[4+i]);
 			}
 		}
 
 		return {
-			data: split[0].split(' ')[0], //obcięcie zbędnej godziny ze stringa daty
-			pesel: split[19], //pierwsze sześć cyfr pesel
-			// pesel: split[19] !== undefined ? split[19].slice(0,6) : '', //pierwsze sześć cyfr pesel
+			data: visit[0].split(' ')[0], //obcięcie zbędnej godziny ze stringa daty
+			pesel: visit[19], //pierwsze sześć cyfr pesel
+			// pesel: visit[19] !== undefined ? visit[19].slice(0,6) : '', //pierwsze sześć cyfr pesel
 			icd10,
-			imie: split[18] !== undefined ? split[18].slice(split[18].lastIndexOf(' ')) : '', //imię - ostatni człon stringa nazwisko i imię
-			nazwisko: split[18] !== undefined ? split[18][0] : '', //pierwsza litera nazwiska
-			personel:  split[20],
-			nazwa: split[3]
+			imie: visit[18] !== undefined ? visit[18].slice(visit[18].lastIndexOf(' ')) : '', //imię - ostatni człon stringa nazwisko i imię
+			nazwisko: visit[18] !== undefined ? visit[18][0] : '', //pierwsza litera nazwiska
+			personel:  visit[20],
+			nazwa: visit[3]
 		}
 	});
-	console.clear();
-	// console.log(JSON.stringify(outputData, undefined, 2));
+
+	return visitsArr;
+}
+
+readDataFromFile('../data/data.csv')
+.then(dataFromFile => {
+	console.log(convertReadDataToVisitsArr(dataFromFile));
+}).catch(err => {
+	console.log(`Nie udało się odczytać pliku. Błąd: ${err}`);
+	console.log(err);
 	
-	// znalezienie wszystkich Z10
-    console.log(outputData.filter(line => line.icd10.includes('Z10')));
-    
 });
