353b0a886a854aab6982e78c6f12f1cd01d9c9f3
Jurek <jurek2006@gmail.com>
2018-04-11 08:53:04 +0200

    Dodana podstawowa route za pomocą express

--- app/app.js
+++ app/app.js
@@ -2,11 +2,12 @@ const _ = require('lodash')
 const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcrypt');
+const express = require('express');
 
 const visits = require('../app/modules/visits'); //moduł do przechowywania danych wizyt
 const {readFile} = require('../app/modules/utils');
 
-
+const app = express();
 
 const splitDataToArr = (readTextData) => {
 // funkcja dzieląca zczytany plik z danymi na tabelę dwuwymiarową
@@ -21,19 +22,50 @@ const findIcd10inVisits = (icd10toFind, visitsArr) => {
 	return visitsArr.filter(visit => visit.icd10.includes(icd10toFind) );
 }
 
-readFile('../../data/data.csv')
-.then(dataFromFile => {
+// readFile('../../data/data.csv')
+// .then(dataFromFile => {
 	
-	const dataRawArr =  splitDataToArr(dataFromFile); //tablica danych wizyty - rozdzielona tylko na tablicę dwuwymiarową
-	visits.importManyFromArray(dataRawArr);
-	// visits.showAll();
-	visits.findMultipleVisitsOfDay();
-	visits.saveAllToJSON();
-	visits.saveReportAsJSON();
+// 	const dataRawArr =  splitDataToArr(dataFromFile); //tablica danych wizyty - rozdzielona tylko na tablicę dwuwymiarową
+// 	visits.importManyFromArray(dataRawArr);
+// 	// visits.showAll();
+// 	visits.findMultipleVisitsOfDay();
+// 	visits.saveAllToJSON();
+// 	visits.saveReportAsJSON();
 	
 	 
-}).catch(err => {
-	console.log(`Nie udało się odczytać pliku. Błąd: ${err}`);
-	console.log(err);
+// }).catch(err => {
+// 	console.log(`Nie udało się odczytać pliku. Błąd: ${err}`);
+// 	console.log(err);
 	
-}); 
+// }); 
+
+app.get('/', (req, res) => {
+	
+	visits.removeAll();
+	readFile('../../data/data.csv')
+		.then(dataFromFile => {
+			
+			const dataRawArr =  splitDataToArr(dataFromFile); //tablica danych wizyty - rozdzielona tylko na tablicę dwuwymiarową
+			visits.importManyFromArray(dataRawArr);
+			// visits.showAll();
+			visits.findMultipleVisitsOfDay();
+			const report = visits.generateReportObj();
+			// visits.saveAllToJSON();
+			// visits.saveReportAsJSON();
+			
+			res.send(report);
+			
+		}).catch(err => {
+			// console.log(`Nie udało się odczytać pliku. Błąd: ${err}`);
+			console.log(err);
+			res.status(400).send(err);
+		}); 
+});
+
+if(!module.parent){
+    app.listen(3000, () => {
+        console.log(`Started on port 3000`);
+    });
+}
+
+module.exports = {app};
