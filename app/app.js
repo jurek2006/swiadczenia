ac00a1a67754d89b322efd22759721f2dd364ec5
Jurek <jurek2006@gmail.com>
2018-04-28 06:09:26 +0200

    Dodana wstępna obsługa ścieżek user input i domyślnych w anonymise

--- app/app.js
+++ app/app.js
@@ -46,6 +46,7 @@ const importAnonymiseAndSave = (pathToFileToAnonymise, pathToSaveAfter) => {
 // -------------------- funkcje do przeniesienia do modułu utils albo anonimizacji
 const readAnonymiseDefaultsPaths = (pathToStoringJSON) => {
 // wczytuje domyślne ścieżki i nazwy plików zdefiniowane dla anonimizacji (zapisane w pliku pathToStoringJSON) 
+		
 
 		// obiekt defaults z niezdefiniowanymi wartościami 
 		let defaultsToSave = {
@@ -56,7 +57,8 @@ const readAnonymiseDefaultsPaths = (pathToStoringJSON) => {
 			output: {
 				defaultPath: undefined,
 				defaultFilename: undefined
-			}
+			},
+			readSuccess: false //przekazuje, czy udało się odczytać plik //TYMCZASOWE ???
 		}
 
 		return readFile(pathToStoringJSON)
@@ -64,8 +66,11 @@ const readAnonymiseDefaultsPaths = (pathToStoringJSON) => {
 			// jeśli udało się odczytać - wczytanie do defaultsToSave wartości odpowiednich pól
 				const read = JSON.parse(res);
 				
+				
+				defaultsToSave.readSuccess = true; //TYMCZASOWE ???
+				
 				if(read.input){
-		
+					
 					if(read.input.defaultPath){
 						defaultsToSave.input.defaultPath = read.input.defaultPath
 					}
@@ -81,14 +86,15 @@ const readAnonymiseDefaultsPaths = (pathToStoringJSON) => {
 					if(read.output.defaultFilename){
 						defaultsToSave.output.defaultFilename = read.output.defaultFilename
 					}
+
 				}
 				
 			})
 			.catch(err => {
 			// jeśli nie udało się wczytać - wyświetlenie w konsoli błędu
-				console.log(`Nie udało się wczytać pliku domyślnych ścieżek/nazw plików: ${err.path}`);
+				console.log(`Nie udało się wczytać pliku: ${err.path}`);
 			}).then(res => {
-			// niezależnie, czy udało się wczytać wartości z pliku, czy nie - zwrócony zostaje obiekt defaultsToSave (gdy nie udało się odczytać - jego wartości są po prostu undefined)
+			// niezależnie, czy udało się wczytać wartości z pliku, czy nie - zwrócony zostaje obiekt defaultsToSave (gdy nie udało się odczytać - readSuccess jest false, a jego wartości są po prostu undefined)
 				return defaultsToSave;
 			});
 
@@ -130,28 +136,28 @@ const argv = yargs
 	// 		string: true
 	// 	}
 	// })
-	.command('anonymise [input] [output] [defaultPaths]', 'Importuje wizyty z pliku csv [input], anonimizuje i zapisuje do pliku csv [output]', (yargs) => {
+	.command('anonymise [input] [output]', 'Importuje wizyty z pliku csv [input], anonimizuje i zapisuje do pliku csv [output]', (yargs) => {
 		yargs
 			.positional('input', {
 				describe: 'Ścieżka względna do pliku csv z wizytami do zanonimizowania (jeśli podano parametr defaultPaths to tylko nazwa pliku)',
 				alias: 'i',
 				type: 'string',
-				default: 'defaultInput'
+				default: 'defaultInput' //TYMCZASOWE
 			})
 			.positional('output', {
 				describe: 'Ścieżka względna do zapisania pliku csv po anonimizacji (jeśli podano parametr defaultPaths to tylko nazwa pliku)',
 				alias: 'o',
 				type: 'string',
-				default: 'defaultOutput'
+				// default: 'default' //TYMCZASOWE
 			})
-			.positional('defaultPaths', {
-				describe: 'Jeśli podany (true) to używane są domyślne ścieżki do odczytania i zapisania plików (bez nazw plików, które są wtedy definiowane w input i output)',
-				alias: ['d', 'dp'],
-				type: 'boolean',
-				default: 'false'
-			})
+			// .positional('defaultPaths', {
+			// 	describe: 'Jeśli podany (true) to używane są domyślne ścieżki do odczytania i zapisania plików (bez nazw plików, które są wtedy definiowane w input i output)',
+			// 	alias: ['d', 'dp'],
+			// 	type: 'boolean',
+			// 	default: 'false'
+			// })
 	})
-	.command('setDefault <type> [path] [filename]', 'Ustawia ścieżkę i/lub nazwę pliku csv do importu wizyt do zanonimizowania (input) czy do zapisania po anonimizacji (output)', (yargs) => {
+	.command('setDefault <type> [path] [filename]', `Ustawia ścieżkę i/lub nazwę pliku csv do importu wizyt do zanonimizowania (input) czy do zapisania po anonimizacji (output) względem ${__dirname}`, (yargs) => {
 		yargs
 		.positional('type', {
 			describe: '<input|output> Określa czy ustawiana domyślna wartość dla input lub output',
@@ -178,85 +184,114 @@ const argv = yargs
 	.alias('help', 'h')
 	.argv;
 
+console.log('argv:', argv);
+
 const argvCommand = argv._[0];
 // jeśli uruchomiono app.js anonymise to za pomocą parametrów: 
-// -s musi być podana ścieżka do pliku z wizytami, 
-// -o może być podana ścieżka do zapisu wizyt po anonimizacji
 
 if(argvCommand === 'anonymise'){
 	console.log('Anonimizacja');
 
-	// // obiekt defaults z niezdefiniowanymi wartościami 
-	// let defaultsToSave = {
-	// 	input: {
-	// 		defaultPath: undefined,
-	// 		defaultFilename: undefined
-	// 	},
-	// 	output: {
-	// 		defaultPath: undefined,
-	// 		defaultFilename: undefined
-	// 	}
-	// }
+	let defaultPaths;
 
-	// // wczytanie defaults
-	// readFile('../config/anonymise_defaults.json')
-	// .then(res => {
-	// 	const read = JSON.parse(res);
+	readAnonymiseDefaultsPaths('../config/anonymise_defaults.json') //wczytuje domyślne ścieżki i nazwy plików zdefiniowane dla anonimizacji 
+	.then(defaultsRed => {
+	// jeśli nie udało się wczytać ścieżek z pliku - także zwraca sukces promisy, tylko ścieżki są ustawione na undefined
+		defaultPaths = defaultsRed;
+		console.log(defaultPaths);
+
+		// ścieżki do faktycznej anonimizacji:
+		let inputPath;
+		let outputPath;
+
+		if(argv.input === 'defaultInput'){
+		// jeśli wybrano anonymise z domyślnym inputem - musi być podana default Path i defaultFilename
+			console.log(defaultPaths.input.defaultPath)// || !!defaultPaths.input.defaultFilename);
+			
+			if( !defaultPaths.input.defaultPath || !defaultPaths.input.defaultFilename ){
+			// jeśli nie udało się odczytać z pliku domyślnej ścieżki lub nazwy pliku dla input - przerwanie działania
+				console.log('Nie zdefiniowano domyślnej ścieżki i/lub nazwy pliku do anonimizacji. Zdefiniuj (za pomocą polecenia node app.js setDefault) aby użyć');
+				return;
+			} else {
+			// jeśli odczytano domyślne inputy - utworzenie ścieżki input z domyślnej ścieżki i domyślnej nazwy pliku
+				inputPath = path.join(defaultPaths.input.defaultPath, defaultPaths.input.defaultFilename); 
+			}
+	
+		} else {
+		//jeśli przekazano coś jako input 
+			if(!defaultPaths.input.defaultPath){
+			// jeśli nie udało odczytać się input.defaultPath - odniesienie podanego inputu do ścieżki aplikacji (położenia app.js)
+				console.log(`Nie zdefiniowano domyślnej ścieżki dla pliku do anonimizacji. Użyta ścieżka aplikacji ${__dirname}`);
+				inputPath = path.join(argv.input);
+			} else {
+
+				// jeśli udało odczytać się input.defaultPath - odniesienie podanego inputu do input.defaultPath
+				inputPath = path.join(defaultPaths.input.defaultPath, argv.input);
+			}
+		}
+
+		if(!argv.output){
+		//jeśli nie podano outputu to uruchamiane jest importAnonymiseAndSave bez parametru pathToSaveAfter
+			outputPath = undefined; //tymczasowe
+		} else if(argv.output === 'default'){
+		// jeśli jako output podano default to brana jest pod uwagę odczytane ścieżka i nazwa domyślna dla output
+
+			if( !defaultPaths.output.defaultPath || !defaultPaths.output.defaultFilename ){
+			// jeśli nie udało się odczytać z pliku domyślnej ścieżki lub nazwy pliku dla output - przerwanie działania
+				console.log('Nie zdefiniowano domyślnej ścieżki i/lub nazwy pliku do zapisania pliku po anonimizacji. Zdefiniuj (za pomocą polecenia node app.js setDefault) aby użyć');
+				return;
+			} else {
+			// jeśli odczytano domyślne output - utworzenie ścieżki output z domyślnej ścieżki i domyślnej nazwy pliku
+				outputPath = path.join(defaultPaths.output.defaultPath, defaultPaths.output.defaultFilename); 
+			}
+		} else {
+		//jeśli przekazano coś jako output 
+		if(!defaultPaths.output.defaultPath){
+			// jeśli nie udało odczytać się output.defaultPath - odniesienie podanego outputu do ścieżki aplikacji (położenia app.js)
+				console.log(`Nie zdefiniowano domyślnej ścieżki dla pliku zapisu po anonimizacji. Użyta ścieżka aplikacji ${__dirname}`);
+				outputPath = path.join(argv.output);
+			} else {
+
+				// jeśli udało odczytać się output.defaultPath - odniesienie podanego outputu do output.defaultPath
+				outputPath = path.join(defaultPaths.output.defaultPath, argv.output);
+			}
+		}
+
+
+		console.log(`Wykonaj anonimizację pliku z ${inputPath} czyli ${path.join(__dirname, inputPath)}`); //TYMCZASOWE
+		if(!outputPath){
+
+			console.log(`Zapisz plik po anonimizacji do domyślnej ścieżki utworzonej przez importAnonymiseAndSave`); //TYMCZASOWE
+		} else {
+
+			console.log(`Zapisz plik po anonimizacji do ${outputPath} czyli ${path.join(__dirname, outputPath)}`); //TYMCZASOWE
+		}
 		
-	// 	if(read.input){
-
-	// 		if(read.input.defaultPath){
-	// 			defaultsToSave.input.defaultPath = read.input.defaultPath
-	// 		}
-	// 		if(read.input.defaultFilename){
-	// 			defaultsToSave.input.defaultFilename = read.input.defaultFilename
-	// 		}
-	// 	}
-
-	// 	if(read.output){
-	// 		if(read.output.defaultPath){
-	// 			defaultsToSave.output.defaultPath = read.output.defaultPath
-	// 		}
-	// 		if(read.output.defaultFilename){
-	// 			defaultsToSave.output.defaultFilename = read.output.defaultFilename
-	// 		}
-	// 	}
-
-	// 	if(argv.defaultPaths){
-	// 		console.log('Domyślne ścieżki');
-	// 		console.log(`Wczytanie pliku ${read.input.defaultPath}/${read.input.defaultFilename}`);
-	// 		console.log(`Zapisanie po anonimizacji do pliku ${read.output.defaultPath}/${read.output.defaultFilename}`);
-	// 		return importAnonymiseAndSave(`${read.input.defaultPath}/${read.input.defaultFilename}`, `${read.output.defaultPath}/${read.output.defaultFilename}`);
-	// 	} 
-		
-	// })
-	// .catch(err => {
-
-	// 	console.log(`Nie udało się wczytać pliku domyślnych ścieżek/nazw plików: ${err}`);
-	// });
+	}) 
+	.catch(err => console.log('Inny błąd:', err)) 
 
 	// ścieżki do faktycznej anonimizacji:
-	let inputPath;
-	let outputPath;
+	// let inputPath;
+	// let outputPath;
 	
-	if(argv.input === 'defaultInput'){
-		console.log('Domyślny input');
-	} else {
-		inputPath = argv.input;
-		console.log(`Input: ${argv.input}`);
-	}
+	// if(argv.input === 'defaultInput'){
+	// 	console.log('Domyślny input');
+	// } else {
+	// 	inputPath = argv.input;
+	// 	console.log(`Input: ${argv.input}`);
+	// }
 	
-	if(argv.output === 'defaultOutput'){
-		inputPath = argv.input;
-		console.log('Domyślny output');
-	} else {
-		outputPath = argv.output;
-		console.log(`Output: ${argv.output}`);
-	}
+	// if(argv.output === 'defaultOutput'){
+	// 	inputPath = argv.input;
+	// 	console.log('Domyślny output');
+	// } else {
+	// 	outputPath = argv.output;
+	// 	console.log(`Output: ${argv.output}`);
+	// }
 
-	importAnonymiseAndSave(inputPath, outputPath)
-	.then(res => console.log(res.message))
-	.catch(err => console.log(err));
+	// importAnonymiseAndSave(inputPath, outputPath)
+	// .then(res => console.log(res.message))
+	// .catch(err => console.log(err));
 	
 } else if (argvCommand === 'setDefault'){
 	if(argv.type !== 'input' && argv.type !== 'output'){
@@ -264,67 +299,13 @@ if(argvCommand === 'anonymise'){
 		return;
 	}
 
-	// obiekt defaults z niezdefiniowanymi wartościami 
-	// let defaultsToSave = {
-	// 	input: {
-	// 		defaultPath: undefined,
-	// 		defaultFilename: undefined
-	// 	},
-	// 	output: {
-	// 		defaultPath: undefined,
-	// 		defaultFilename: undefined
-	// 	}
-	// }
-
-	// // wczytanie dotychczasowych defaults
-	// readFile('../config/anonymise_defaults.json')
-	// 	.then(res => {
-	// 		const read = JSON.parse(res);
-			
-	// 		if(read.input){
-
-	// 			if(read.input.defaultPath){
-	// 				defaultsToSave.input.defaultPath = read.input.defaultPath
-	// 			}
-	// 			if(read.input.defaultFilename){
-	// 				defaultsToSave.input.defaultFilename = read.input.defaultFilename
-	// 			}
-	// 		}
-
-	// 		if(read.output){
-	// 			if(read.output.defaultPath){
-	// 				defaultsToSave.output.defaultPath = read.output.defaultPath
-	// 			}
-	// 			if(read.output.defaultFilename){
-	// 				defaultsToSave.output.defaultFilename = read.output.defaultFilename
-	// 			}
-	// 		}
-
-			
-	// 	})
-	// 	.catch(err => {
-
-	// 		console.log(`Nie udało się wczytać pliku domyślnych ścieżek/nazw plików: ${err} \r\nWczytano puste`);
-	// 	}).then(res => {
-
-	// 		if(argv.path){
-	// 			defaultsToSave[argv.type].defaultPath = argv.path;
-	// 		}
-
-	// 		if(argv.filename){
-	// 			defaultsToSave[argv.type].defaultFilename = argv.filename;
-	// 		}
-
-	// 		return saveJSON(defaultsToSave, '../config/anonymise_defaults.json')
-	// 	}).then(res => console.log(res))
-	// 	.catch(err => console.log(err));
-
 	let defaultsToSave;
 
 	readAnonymiseDefaultsPaths('../config/anonymise_defaults.json') //wczytuje domyślne ścieżki i nazwy plików zdefiniowane dla anonimizacji 
 	.then(defaultsRed => {
+	// jeśli się powiodło wczytanie ścieżek z pliku - przekazuje je do defaultsToSave
 		defaultsToSave = defaultsRed;
-	}) //jeśli się powiodło - wyświetla je
+	}) 
 	.catch(err => console.log(err)) //przechwycenie błędów odczytania dotychczasowych domyślnych ścieżek
 	.then(res => { 
 
@@ -337,14 +318,22 @@ if(argvCommand === 'anonymise'){
 		}
 
 		return saveJSON(defaultsToSave, '../config/anonymise_defaults.json')
-	}).then(res => console.log(res))
+	}).then(res => {
+		console.log(`Zapisano domyślne ścieżki anonimizacji w pliku ${res.fileSaved}:`);
+		console.log(defaultsToSave);
+		
+	})
 	.catch(err => console.log(err));
 		
 } else if (argvCommand === 'showDefault'){
 	console.log('Domyślne ścieżki i nazwy plików używane w anonimizacji:');
 
 	readAnonymiseDefaultsPaths('../config/anonymise_defaults.json') //wczytuje domyślne ścieżki i nazwy plików zdefiniowane dla anonimizacji 
-	.then(defaults => console.log(defaults)) //jeśli się powiodło - wyświetla je
+	.then(defaults => {
+		console.log(defaults);
+		console.log(`Domyślna ścieżka input: ${path.join(__dirname, defaults.input.defaultPath)}`);
+		
+	}) //jeśli się powiodło - wyświetla je
 	.catch(err => console.log(err.message));
 	
 } else {
